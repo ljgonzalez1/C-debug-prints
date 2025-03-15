f2a1c59264c492b10797ad0d95e62f553cbbc798
Luis González <ljgonzalez@uc.cl>
2025-03-14 21:04:58 -0300

    🔥 remove(fibonacci): delete unused Fibonacci sequence implementation and header files

--- src/fibonacci/fibonacci.h
+++
@@ -1,8 +0,0 @@
-#ifndef FIBONACCI_H
-#define FIBONACCI_H
-
-void run_fibonacci(int n);
-void run_short_sequence(void);
-void run_fibonacci_examples(void);
-
-#endif // FIBONACCI_H
--- src/main.c
+++ src/main.c
@@ -1,16 +1,14 @@
 #include "debug/debug.h"
-#include "fibonacci/fibonacci.h"
 
 int main(void) {
     init_debug();
-
-    // run_fibonacci_examples();
 
     message(DEBUG_DEBUG, "TEST", "1");
     message(DEBUG_INFO, "TEST", "2");
     message(DEBUG_WARN, "TEST", "3");
     message(DEBUG_ERROR, "TEST", "");
     message(DEBUG_FATAL, "TEST", "5");
+    message(DEBUG_FATAL, "", "6");
 
     return 0;
 }
