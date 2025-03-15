6c6dda50fb6d8f51b1ae6314393e054373211cf4
Luis González <ljgonzalez@uc.cl>
2025-03-14 21:00:20 -0300

    ♻️  refactor(printer.c): rename variables and remove comments for clarity in debug print functions

--- src/debug/printer.c
+++ src/debug/printer.c
@@ -32,9 +32,9 @@ static char* my_strdup(const char* s) {
 // -------------------------------------------------------------------------
 // Formatting
 // -------------------------------------------------------------------------
-static void format_time(double t, char *dst, size_t size) {
+static void format_time(double time_elapsed, char *dst, size_t size) {
     char temp[32];
-    snprintf(temp, sizeof(temp), "%.6f", t); // p.ej. "1.234567", "12.345678"
+    snprintf(temp, sizeof(temp), "%.6f", time_elapsed);
 
     size_t len = strlen(temp);
     if (len == 0) {
@@ -51,9 +51,7 @@ static void format_time(double t, char *dst, size_t size) {
     }
 }
 
-// -------------------------------------------------------------------------
-// Determina la cadena con el tipo y espacios adecuados
-// -------------------------------------------------------------------------
+
 static const char* get_type_suffix(DebugType type) {
     switch (type) {
         case DEBUG_INFO:  return "INFO :";
@@ -66,8 +64,7 @@ static const char* get_type_suffix(DebugType type) {
 }
 
 // -------------------------------------------------------------------------
-// Imprime un texto 'msg' en la PRIMERA línea con prefijo "prefix_ansi",
-// alineando líneas siguientes (si msg tiene \n).
+// Printing
 // -------------------------------------------------------------------------
 static void print_multiline_msg(const char* msg,
                                 const char* prefix_ansi,
@@ -85,18 +82,12 @@ static void print_multiline_msg(const char* msg,
     int line_idx = 0;
     while (line) {
         if (line_idx == 0) {
-            // Primera línea => prefix + line
-            if (debug_colors_supported) {
-                // color ya está re-aplicado en prefix_ansi, pero reafirmamos
-                printf("%s%s\n", prefix_ansi, line);
-                // no se hace un reset total, porque prefix_ansi ya incluyó el color
-            } else {
-                // sin color
-                printf("%s%s\n", prefix_ansi, line);
-            }
+            printf("%s%s\n", prefix_ansi, line);
+
         } else {
-            // Subsiguientes => prefix_len espacios + color + line
+            // Next lines => prefix_len spaces + color + line
             char* spaces = malloc(prefix_len + 1);
+
             if (spaces) {
                 memset(spaces, ' ', prefix_len);
                 spaces[prefix_len] = '\0';
@@ -109,6 +100,7 @@ static void print_multiline_msg(const char* msg,
                 free(spaces);
             }
         }
+
         line = strtok_r(NULL, "\n", &saveptr);
         line_idx++;
     }
@@ -116,11 +108,6 @@ static void print_multiline_msg(const char* msg,
     free(copy);
 }
 
-// -------------------------------------------------------------------------
-// Imprime 'detail' alineado EXACTAMENTE bajo el msg, con cursiva
-// en la primera línea también (sin repetir prefix).
-// => line_idx=0 => ya es una "línea subsecuente" => 'prefix_len' espacios
-// -------------------------------------------------------------------------
 static void print_detail(const char* detail,
                          int prefix_len,
                          const char* color)
@@ -137,6 +124,7 @@ static void print_detail(const char* detail,
     while (line) {
         // Cada línea => prefix_len espacios + color + italics + line
         char* spaces = malloc(prefix_len + 1);
+
         if (spaces) {
             memset(spaces, ' ', prefix_len);
             spaces[prefix_len] = '\0';
@@ -146,7 +134,7 @@ static void print_detail(const char* detail,
                 printf("%s%s%s%s\n", spaces, color, ANSI_ITALIC, line);
                 printf("%s", ANSI_RESET);
             } else {
-                // sin color
+                // no color
                 printf("%s%s\n", spaces, line);
             }
             free(spaces);
@@ -158,24 +146,21 @@ static void print_detail(const char* detail,
 }
 
 // -------------------------------------------------------------------------
-// Función principal: print_debug
-// => Muestra [  0.123456 ] TIPO: Mensaje
-//    y el detail alineado en cursiva
+// Main function: print_debug
+// => Shows: [  0.123456 ] TYPE: message
+//                               and the details in italics
 // -------------------------------------------------------------------------
 void print_debug(DebugType type, const char *msg, const char *detail) {
-    double t = get_elapsed_time();
+    double time_elapsed = get_elapsed_time();
 
-    // 1) Arma el tiempo => "  1.234567", etc.
     char timebuf[32];
-    format_time(t, timebuf, sizeof(timebuf));
+    format_time(time_elapsed, timebuf, sizeof(timebuf));
     // => "[  1.234567 ]"
     char bracketed[64];
     snprintf(bracketed, sizeof(bracketed), "[ %s ]", timebuf);
 
-    // 2) Obtiene TIPO => "ERR  :", "INFO :", etc.
     const char* type_str = get_type_suffix(type);
 
-    // 3) Determina color
     const char* color = "";
     if (debug_colors_supported) {
         switch (type) {
@@ -188,29 +173,23 @@ void print_debug(DebugType type, const char *msg, const char *detail) {
         }
     }
 
-    // 4) prefix_plain (sin secuencias ANSI) => p.ej. "[   0.123456 ] ERR  : "
     char prefix_plain[256];
     snprintf(prefix_plain, sizeof(prefix_plain), "%s %s ", bracketed, type_str);
     int prefix_len = (int)strlen(prefix_plain);
 
-    // 5) prefix_ansi => con color y bold en type, luego se retoma color normal
-    //    para el resto del mensaje.
     char prefix_ansi[512];
     if (debug_colors_supported && *color) {
-        // Ej: color + bracketed + " " + BOLD + type + RESET + color + " "
         snprintf(prefix_ansi, sizeof(prefix_ansi),
                  "%s%s %s%s%s%s ",
                  color, bracketed,
                  ANSI_BOLD, type_str,
                  ANSI_RESET, color);
     } else {
-        // sin color
+        // no color
         snprintf(prefix_ansi, sizeof(prefix_ansi), "%s %s ", bracketed, type_str);
     }
 
-    // 6) Imprimir el msg principal multiline
     print_multiline_msg(msg, prefix_ansi, prefix_len, color);
 
-    // 7) Imprimir detail, alineado, en cursiva (y sin repetir prefix).
     print_detail(detail, prefix_len, color);
 }
